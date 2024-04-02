3b35fb2f3f786419fa03e1537d5bac83f88f9168
neeleshseerapu <145879899+neeleshseerapu@users.noreply.github.com>
2024-04-01 19:58:58 -0700

    Changing the starter message.

--- text_to_speech/Speech_Adventure.py
+++ text_to_speech/Speech_Adventure.py
@@ -2,5 +2,5 @@ from textToSpeech import TTS
 
 neelesh = TTS(rate=160)
 
-neelesh.speak("Hello my dear family!")
+neelesh.speak("Hello world!")
 neelesh.speak("I have created a module that uses 'p y t t s x 4', to convert text to speech without much hassle!")
